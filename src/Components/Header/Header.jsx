194cd27fa4d924c01dd84841e4f134f97b831353
codewitHK1 <kumarhk1002@gmail.com>
2024-02-02 22:00:09 +0530

    header completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import emailIcon from "../../assets/icons/email_icon.png";
 import contactIcon from "../../assets/icons/contact_icon.png";
+import logo from "../../assets/icons/buildx_logo.png";
+import searchIcon from "../../assets/icons/search_icon.png";
 
 const Header = () => {
   return (
@@ -18,7 +20,27 @@ const Header = () => {
           </span>
         </div>
       </div>
-      <div></div>
+      <div className="flex">
+        <div className="flex w-[40%] justify-center items-center">
+          <img src={logo} alt="buildx-logo" />
+        </div>
+        <div className="flex w-[60%] justify-center items-center">
+          <nav>
+            <ul className="flex gap-5">
+              <li className="font-semibold cursor-pointer text-[#F9B701]">
+                About
+              </li>
+              <li className="font-semibold cursor-pointer">Featured</li>
+              <li className="font-semibold cursor-pointer">Projects</li>
+              <li className="font-semibold cursor-pointer">Pages</li>
+              <li className="font-semibold cursor-pointer">Contact</li>
+              <li className="font-semibold cursor-pointer mt-1">
+                <img src={searchIcon} alt="search" />
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </header>
   );
 };
